b490ee343ba5c7809da5deb66403cbef19ff62e3
antosova <116720921+antosova@users.noreply.github.com>
2022-11-06 00:04:44 +0100

    Oprava

--- 1.du_antosova.py
+++ 1.du_antosova.py
@@ -1,18 +1,23 @@
 #program pro hraní piškvorek na libovolně velké hrací ploše tvořené z šestiúhelníků. Velikost strany šestiúhelníku je 50 jednotek.
 #uživatel zadává souřadnice od 1 do a (a = počet sloupců) a od 1 do b (b = počet řádků). Souřadnice [1,1] je v levém horním poli.
 
-from turtle import forward, left, right, penup, pendown, speed, setpos, ycor, pensize, circle
+from turtle import forward, left, right, penup, pendown, speed, setpos, ycor, pensize, circle, exitonclick
 
-a = int(input("Zadejte počet sloupců:"))                                                  #uživatel si zvolí velikost hrací plochy
-b = int(input("Zadejte počet řádků:"))
+sloupce = int(input("Zadejte počet sloupců:"))                                            #uživatel si zvolí velikost hrací plochy
+radky = int(input("Zadejte počet řádků:"))
 
-speed(0)                                                                                  #rychlost vykreslování
+while sloupce <=0 or radky <=0:
+    print("Chybný vstup, zadejte kladné číslo")
+    sloupce = int(input("Zadejte počet sloupců:"))                                       
+    radky = int(input("Zadejte počet řádků:"))      
+       
+speed(0)                                                                                  
 
-pensize(5)                                                                                #vykreslení mřížky pro přehlednost tlustší linií
+pensize(5)                                                                                
 
 left(30)                                                                                  #vykreslení šestiúhelníkové sítě
-for i in range (b):
-    for i in range (a):
+for i in range (radky):
+    for i in range (sloupce):
         for i in range(8):
             forward(50)
             right(60)
@@ -23,30 +28,29 @@ for i in range (b):
 right(60)
 
 
-pensize(2)                                                                                #vykreslení symbolů pro přehlednost užší linií
+pensize(2)                                                                                
 
 pocitadlo = 0                                                                                            
-while pocitadlo < (a*b):                                                                  #program se ptá uživalů na souřadnice vykreslení symbolů do té doby, než zaplní hrací plochu
-    pocitadlo +=1                                                                         #počítadlo úspěšného zadání souřadnic
+while pocitadlo < (sloupce*radky):                                                        #program se ptá uživalů na souřadnice vykreslení symbolů do té doby, než zaplní hrací plochu
+    pocitadlo +=1                                                                         
     
-    if pocitadlo % 2 == 1:                                                                #začíná hráč s křížky
+    if pocitadlo % 2 == 1:                                                                
         print("Nyní hraje hráč za křížky.")
         
-        x = int(input("Zadejte souřadnici x: "))                                          #program se zeptá uživatele, kam si přeje zakreslit křížek
+        x = int(input("Zadejte souřadnici x: "))                                          
         
-        if x <= 0 or x > a:                                                               #pokud uživatel zadá souřadnice mimo hrací plochu, program ho na to upozorní a nechá zadat znovu
+        if x <= 0 or x > sloupce:                                                          
             print("Byly zadány souřadnice mimo hrací pole, zadejte znovu.")
             pocitadlo -=1
             continue
 
         y = int(input("Zadejte souřadnici y: "))
         
-        if y <= 0 or y > b:
+        if y <= 0 or y > radky:
             print("Byly zadány souřadnice mimo hrací pole, zadejte znovu.")
             pocitadlo -=1
             continue
-
-        penup()                                                                           #vykreslení křížku     
+        penup()                                                                            #vykreslení křížku     
         setpos((x-1)*86.6, (y-1)*(-100))
         pendown()
         forward(100)
@@ -60,22 +64,22 @@ while pocitadlo < (a*b):                                                        
         
             
     else:
-        print ("Nyní hraje hráč za kolečko.")                                              #jako druhý hraje hráč s kolečky
-        x = int(input("Zadejte souřadnici x: "))                                           #program se zeptá uživatele, kam si přeje zakreslit kolečko
+        print ("Nyní hraje hráč za kolečko.")                                              
+        x = int(input("Zadejte souřadnici x: "))                                           
                                                                                                             
-        if x <= 0 or x > a:                                                                #pokud uživatel zadá souřadnice mimo hrací plochu, program ho na to upozorní a nechá zadat znovu (while ciklus se spustí znovu)
+        if x <= 0 or x > sloupce:                                                                
             print("Byly zadány souřadnice mimo hrací pole, zadejte znovu.")
             pocitadlo -=1
             continue
 
         y = int(input("Zadejte souřadnici y: "))
         
-        if y <= 0 or y > b:
+        if y <= 0 or y > radky:
             print("Byly zadány souřadnice mimo hrací pole, zadejte znovu.")
             pocitadlo -=1
             continue 
 
-        penup()                                                                             #vykreslení křížku na uživalelem zvolenou pozici
+        penup()                                                                             #vykreslení kolečka
         setpos((x-1)*86.6, (y-1)*(-100))
         right(39)
         forward(40)
